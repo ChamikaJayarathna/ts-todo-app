4a5dbb648b9bafe7c95d6623a111c0fc65fef87f
Chamika Jayarathna <failgames.lk@gmail.com>
2026-01-29 13:20:06 +0530

    feat: add styles.css file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './style.css';
+
 const todoInput = document.getElementById("todo-text") as HTMLInputElement;
 const todoAdd = document.getElementById("todo-add") as HTMLButtonElement;
 const todoList = document.getElementById("todo-list") as HTMLUListElement;
